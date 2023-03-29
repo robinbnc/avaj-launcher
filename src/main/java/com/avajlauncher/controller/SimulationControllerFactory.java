f64050ef7b2a504f696ae1018b538c10edab5b1b
BICANIC <robin.bicanic@cgi.com>
2023-03-29 12:18:44 +0200

    Feat: refacto SimulationControllerFactory

--- src/main/java/com/avajlauncher/controller/SimulationControllerFactory.java
+++ src/main/java/com/avajlauncher/controller/SimulationControllerFactory.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+import com.avajlauncher.exceptions.ConfigExceptions;
 import com.avajlauncher.exceptions.IncorrectAircraftType;
 import com.avajlauncher.model.aircraft.Aircraft;
 import com.avajlauncher.model.aircraft.AircraftFactory;
@@ -14,9 +15,9 @@ import com.avajlauncher.model.configFileParser.ConfigParser;
 import com.avajlauncher.model.tower.WeatherTower;
 
 public class SimulationControllerFactory {
-	private String				configFile;
-	private ConfigParser		configParser;
-	private ConfigError			configError;
+	private final String		configFile;
+	private final ConfigParser	configParser;
+	private final ConfigError	configError;
 
 	public SimulationControllerFactory(String p_configFile) {
 		this.configFile = p_configFile;
@@ -28,113 +29,86 @@ public class SimulationControllerFactory {
 	newSimulationController()
 	throws Exception
 	{
+		BufferedReader reader = new BufferedReader(this.openFile());
 		try {
-			FileReader fileReader = this.openFile();
-			BufferedReader reader = new BufferedReader(fileReader);
 			WeatherTower weatherTower = new WeatherTower();
-			SimulationController simulationController;
-	
-			simulationController =
+			SimulationController simulationController =
 				new SimulationController(this.getSimulationTime(reader), weatherTower);
+
 			this.registerAllAircraftInTower(reader, weatherTower);
 			this.closeReader(reader);
 			return (simulationController);
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			throw e;
+		} finally {
+			this.closeReader(reader);
 		}
  	}
 
-	private FileReader openFile() throws FileNotFoundException {
-		try {
-			FileReader fileReader = new FileReader(this.configFile);
-			return (fileReader);
-		}
-		catch (FileNotFoundException e) {
-			throw e;
-		}
-	}
+	private FileReader
+	openFile()
+	throws FileNotFoundException
+	{ return (new FileReader(this.configFile)); }
 
 	private int
 	getSimulationTime(final BufferedReader p_reader)
 	throws Exception
 	{
-		try {
-			String line = p_reader.readLine();
-			
-			this.configError.setLine(line);
-			if (line == null || !this.configError.isSimulationTimeLineCorrect()) {
-				throw new Exception("Error: Incorrect simulation time line.");
-				// personal exception
-			}
-			this.configParser.setLine(line);
-			return (this.configParser.getSimulationTime());
+		String line = p_reader.readLine();
+		
+		this.configError.setLine(line);
+		if (line == null || !this.configError.isSimulationTimeLineCorrect()) {
+			throw new ConfigExceptions("Error: Incorrect simulation time line.");
 		}
-		catch (IOException e) {
-			this.closeReader(p_reader);
-			throw e;
-		}
+		this.configParser.setLine(line);
+		return (this.configParser.getSimulationTime());
 	}
 
 	private void
 	registerAllAircraftInTower(final BufferedReader p_reader, WeatherTower p_weatherTower)
 	throws Exception
 	{
-		try {
-			String line = p_reader.readLine();
-			
-			if (line == null) {
-				// personal exception
-				throw new Exception("Error: No aircraft Provided.");
-			}
-			while (line != null) {
-				this.configError.setLine(line);
-				this.configError.tokenizeAirCraftLine();
-				if (!this.configError.isAircraftLineCorrect()) {
-					// personal exception
-					throw new Exception("Error: Incorrect Aircraft line.");
-				}
-				this.configParser.setLine(line);
-				this.configParser.tokenizeAirCraftLine();
-				this.registerAircraftInTower(p_weatherTower);
-				line = p_reader.readLine();
-			}
+		String line = p_reader.readLine();
+		
+		if (line == null) {
+			throw new ConfigExceptions("Error: No aircraft Provided.");
 		}
-		catch (Exception e) {
-			this.closeReader(p_reader);
-			throw e;
-		} 
+		while (line != null) {
+			this.configError.setLine(line);
+			this.configError.tokenizeAirCraftLine();
+			if (!this.configError.isAircraftLineCorrect()) {
+				throw new ConfigExceptions("Error: Incorrect Aircraft line.");
+			}
+			this.configParser.setLine(line);
+			this.configParser.tokenizeAirCraftLine();
+			this.registerAircraftInTower(p_weatherTower);
+			line = p_reader.readLine();
+		}
 	}
 
 	private void
 	registerAircraftInTower(WeatherTower p_weatherTower)
 	throws IncorrectAircraftType
 	{
-		try {
-			Flyable newAircraft =
-				AircraftFactory.getInstance()
-				.newAircraft(
-					this.configParser.getAircraftType(),
-					this.configParser.getAircraftName(),
-					this.configParser.getAircraftCoordinates()
-			);
-			p_weatherTower.register(newAircraft);
-			System.out.println("Tower says: "
-				+ ((Aircraft)newAircraft).getIndentifier()
-				+ " registered to weather tower."
-			);
-			newAircraft.registerTower(p_weatherTower);
-		}
-		catch (IncorrectAircraftType e) {
-			throw e;
-		}
+		Flyable newAircraft =
+			AircraftFactory.getInstance()
+			.newAircraft(
+				this.configParser.getAircraftType(),
+				this.configParser.getAircraftName(),
+				this.configParser.getAircraftCoordinates()
+		);
+		p_weatherTower.register(newAircraft);
+		System.out.println("Tower says: "
+			+ ((Aircraft)newAircraft).getIndentifier()
+			+ " registered to weather tower."
+		);
+		newAircraft.registerTower(p_weatherTower);
 	}
 
 	private void closeReader(final BufferedReader reader) {
 		try {
 			reader.close();
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			System.out.println(e.getMessage());
 			System.exit(1);
 		}
--- src/main/java/com/avajlauncher/exceptions/CoordinatesExceptions.java
+++
@@ -1,7 +0,0 @@
-package com.avajlauncher.exceptions;
-
-public class CoordinatesExceptions extends Exception { 
-    public CoordinatesExceptions(String errorMessage) {
-        super(errorMessage);
-    }
-}
